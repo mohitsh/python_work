214f21d8844207064caa26b27bff4a8a0fb0bb91
mohitsh <mohitsh114@gmail.com>
2015-08-12 09:30:18 +0530

    binary_search12_08.py

---
+++ linear_data_structure/binary_search12_08.py
@@ -0,0 +1,28 @@
+
+def binary_search(alist, item):
+	found = False
+	first = 0
+	last = len(alist) -1
+	
+	while not found and first <= last:
+		mid = (first+last)/2
+		if item == alist[mid]:
+			found = True
+		else:
+			if item < alist[mid]:
+				last = mid -1 
+			else:
+				first = mid + 1
+	return found
+
+alist = [1,2,3,4,5,6]
+item1 = 1
+item2 = 4
+item3 = 6
+item4 = 32
+
+print binary_search(alist, item1)
+print binary_search(alist, item2)
+print binary_search(alist, item3)
+print binary_search(alist, item4)
+
