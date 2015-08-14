4d456e207c944d41836824ec2698fc855ccdb23e
mohitsh <mohitsh114@gmail.com>
2015-08-14 20:57:02 +0530

    insert_sort141.py

---
+++ sorting_algo/insert_sort141.py
@@ -0,0 +1,22 @@
+
+
+def insert_sort(alist):
+		
+		for index in range(1, len(alist)):
+			
+			position = index
+			currentValue = alist[position]
+			
+			while position > 0 and alist[position - 1] > currentValue:
+				
+				alist[position] = alist[position-1]
+				position = position - 1
+		
+			alist[position] = currentValue
+		return alist
+
+alist = [54,26,93,17,77,31,44,55,20]
+
+print alist
+print insert_sort(alist)
+
