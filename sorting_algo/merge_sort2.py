228f87ba3eb4a3154ec15fb5326921c844ffc669
mohitsh <mohitsh114@gmail.com>
2015-08-17 01:03:36 +0530

    merge_sort2.py

--- sorting_algo/merge_sort2.py
+++ sorting_algo/merge_sort2.py
@@ -1,37 +1,40 @@
-def merge_sort(a):
-	print 'splitting >',a
-	if len(a)>1:
-		mid = len(a)//2
-		lefthalf = a[:mid]
-		righthalf = a[mid:]
-		
-		merge_sort(lefthalf)
-		merge_sort(righthalf)
+
+
+def merge_sort(alist):
 	
+	if len(alist) > 1:
+		mid = len(alist)//2
+		left = alist[mid:]
+		right = alist[:mid]
+		
+		merge_sort(left)
+		merge_sort(right)
+		
 		i = 0
 		j = 0
 		k = 0
-		while i<len(lefthalf) and j<len(righthalf):
-			if lefthalf[i] < righthalf[j]:
-				a[k] = lefthalf[i]
-				i = i+1
+		while i < len(left) and j < len(right):
+			if left[i] < right[j]:
+				alist[k] = left[i]
+				i = i + 1
 			else:
-				a[k] = righthalf[j]
+				alist[k] = right[j]
 				j = j + 1
-			k = k+1
-		
-		while i<len(lefthalf):
-			a[k] = lefthalf[i]
+			k = k + 1
+			
+		while i < len(left):
+			alist[k] = left[i]
 			i = i + 1
 			k = k + 1
-		while j<len(righthalf):
-			a[k] = righthalf[j]
+			
+		while j < len(right):
+			alist[k] = right[j]
 			j = j + 1
 			k = k + 1
+		
+	print 'mergin', alist
 
-	print 'merging >',a
 
-a = [54,26,93,17,77,31,44,55,20]
-merge_sort(a)
-
- 
+alist = [54,26,93,17,77,31,44,55,20]
+merge_sort(alist)
+print(alist)
