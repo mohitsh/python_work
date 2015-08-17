d44bb4e9ae062d32ef5d47991e44c27a57ff36ef
mohitsh <mohitsh114@gmail.com>
2015-08-17 12:47:16 +0530

    quick_sort3.py

--- sorting_algo/quick_sort3.py
+++ sorting_algo/quick_sort3.py
@@ -1,9 +1,20 @@
 
+
+def quicksort(a):
+	quicksorthelper(a,0,len(a)-1)
+	
+
+def quicksorthelper(a,first,last):
+	print 'sorted', a
+	if first < last:
+		split = partition(a,first,last)
+		quicksorthelper(a,first,split-1)
+		quicksorthelper(a,split+1,last)
+
 def partition(a,first,last):
 	pivot = first
 	leftmark = first + 1
 	rightmark = last
-	
 	done = False
 	
 	while not done:
@@ -11,20 +22,23 @@ def partition(a,first,last):
 			leftmark = leftmark + 1
 		while rightmark >= leftmark and a[rightmark] >= a[pivot]:
 			rightmark = rightmark - 1
-
-		if rightmark > leftmark:
+	
+		if rightmark < leftmark:
 			done = True
+
 		else:
 			tmp = a[leftmark]
 			a[leftmark] = a[rightmark]
 			a[rightmark] = tmp
 
-		tmp = a[first] 
-		a[first] = a[rightmark]
-		a[rightmark] = tmp
-	
-	return rightmark 
+	tmp = a[first]
+	a[first] = a[rightmark]
+	a[rightmark] = tmp
 
-a = [54,26,93,17,77,31,44,55,20]
-index = partition(a,0,8) 
-print index, a[index]
+	return rightmark
+
+
+alist = [9,7,6,5,4,3,2,1,0]
+print alist
+quicksort(alist)
+print alist
