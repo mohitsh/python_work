b0b5c4772f7ae964fb7e199cfb8ac392caf8ad6b
mohitsh <mohitsh114@gmail.com>
2015-08-18 10:45:24 +0530

    tree

---
+++ tree/tree1.py
@@ -0,0 +1,28 @@
+
+
+
+myTree = ['a',
+		['b',
+			['d', [],[]],
+			['e',[],[]]],
+		['c',
+			['f', [],[]],
+			[]]
+	]
+
+
+tree = ['root', ['b',['d',[],[]],['e',[],[]]],
+		['c',['f',[],[]],[]]
+	]
+
+
+
+
+print tree[0]
+print tree[1]
+print tree[2]
+
+
+#print myTree[0]
+#print myTree[1]
+#print myTree[2]
