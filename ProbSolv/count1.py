a8bbc98ab63a9572bbd1f2d85ef61d2c0ec5f459
mohitsh <mohitsh114@gmail.com>
2015-08-25 20:09:33 +0530

    count1.py

--- ProbSolv/count1.py
+++ ProbSolv/count1.py
@@ -8,21 +8,30 @@ def count(alist,n):
 			if i == alist[j]:
 				count = count + 1
 		arr.insert(i,count)
-	#arr.sort()
-	#ans = []
-	#for k in arr:
-	#	ans.append(alist[k])
-	#arr.sort()
-	#print ' '.join(str(z) for z in arr)
-	print arr
+
+	#print arr
 	ans = []
-	for k in range(len(arr)):
-		for m in range(arr[k]):
-			ans.append(k)
-	print ' '.join(str(j) for j in ans)	
+	count = 0
+	for m in range(len(arr)):
+		count = count + arr[m]
+		ans.append(count)
+	print ' '.join(str(x) for x in ans)
+	#ans = []
+	#for k in range(len(arr)):
+	#	for m in range(arr[k]):
+	#		ans.append(k)
+	#print ' '.join(str(j) for j in ans)	
 		
 
 n = int(raw_input())
-a = raw_input().split()
-alist = [int(x) for x in a]
-count(alist,n) 
+num = []
+word = []
+for i in range(n):
+	a = raw_input().split()
+	num.append(int(a[0]))	
+	word.append(a[1])
+
+print num
+print word
+#alist = [int(x) for x in a]
+count(num,n) 
