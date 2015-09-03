e17407442074a1cefbc052c9db73c3e027456505
mohitsh <mohitsh114@gmail.com>
2015-09-03 13:04:09 +0530

    repBin_search.py

---
+++ ProbSolv/repBin_search.py
@@ -0,0 +1,27 @@
+
+
+def bin_search(alist,item):
+	first = 0
+	last = len(alist)-1
+	pos = 0
+	found = False
+	while first <= last and not found:
+		mid = (first + last)/2
+		if item < alist[mid]:
+			last = mid-1
+		elif item > alist[mid]:
+			first = mid+1
+		elif item == alist[mid]:
+			found = True
+	
+	if found == False:
+		return found,-1
+	else:
+		return found,mid
+
+
+alist = [1,3,5,8,12,15,17,19,22,24,26,28,31]
+print bin_search(alist,8)
+print bin_search(alist,22)
+print bin_search(alist,31)
+print bin_search(alist,100)
