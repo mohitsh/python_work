2a59afe58df4a9262573b510b0cc12172b73dd23
mohitsh <mohitsh114@gmail.com>
2015-09-14 18:51:43 +0530

    repeat/bubble1.py

---
+++ sorting_algo/repeat/passarr.py
@@ -0,0 +1,21 @@
+
+def reassign(alist):
+	alist = [0,1]
+	
+def append(alist):
+	alist.append(1)
+
+alist = [0]
+print 'original list ', alist
+reassign(alist)
+print 'after reassigning pass ', alist
+append(alist)
+print 'after append pass ', alist
+append(alist)
+print 'after append pass ', alist
+append(alist)
+print 'after append pass ', alist
+append(alist)
+print 'after append pass ', alist
+append(alist)
+print 'after append pass ', alist
