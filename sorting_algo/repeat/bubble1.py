7dbe7a469dd6acd9de8f517e9053e849504f1b2c
mohitsh <mohitsh114@gmail.com>
2015-09-14 15:41:30 +0530

    bubble1.py

--- sorting_algo/repeat/bubble1.py
+++ sorting_algo/repeat/bubble1.py
@@ -1,8 +1,8 @@
 
 
 def bubble(alist):
-	for i in range(len(alist)-1):
-		for j in range(1,len(alist)):
+	for i in range(len(alist),0,-1):
+		for j in range(1,i):
 			if alist[j-1] > alist[j]:
 				temp = alist[j-1]
 				alist[j-1] = alist[j]
@@ -14,3 +14,4 @@ def bubble(alist):
 alist = [6,5,4,3,2,1]
 print alist
 print bubble(alist)
+
