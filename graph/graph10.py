2b6f68a5bb31a00dd0ffcd26a735b588daefb32c
mohitsh <mohitsh114@gmail.com>
2015-09-21 00:58:33 +0530

    graph10.py

--- graph/graph10.py
+++ graph/graph10.py
@@ -18,6 +18,8 @@ class Graph:
 			return self._destination if self._origin else self._origin 
 		def edges(self):
 			return (self._origin,self._destination)
+		def __str__(self):
+			return '({0},{1},{2})'.format(self._origin,self._destination,self._element)
 
 	def __init__(self,directed = False):
 		self._outgoing = {}
@@ -31,6 +33,7 @@ class Graph:
 		self._outgoing[v] = {}
 		if self.is_directed():
 			self._incoming = {}
+		return v
 
 	def insert_edge(self,u,v,x):
 		e = self.Edge(u,v,x)
@@ -45,7 +48,7 @@ class Graph:
 
 	def edge_count(self):
 		total = sum(len(self._outgoing[key]) for key in self._outgoing)
-		return total if self.is_directed else total//2		
+		return total if self.is_directed() else total//2		
 	def edges(self):
 		result = set()
 		for secmap in self._outgoing.values():
@@ -79,5 +82,23 @@ verts = g.vertices()
 for v in verts:
         print v
 
+print "checking for methods:"
+print g.vertex_count()
+v = g.vertices()
+for i in v:
+	print i.element()
 
+e1 = g.insert_edge(v1,v2,10)
+e2 = g.insert_edge(v2,v4,20)
+e3 = g.insert_edge(v1,v4,30)
+e4 = g.insert_edge(v1,v5,40)
+e5 = g.insert_edge(v1,v3,50)
+e6 = g.insert_edge(v3,v5,60)
+e7 = g.insert_edge(v4,v5,70)
+
+print g.edge_count()
+
+e = g.edges()
+for k in e:
+	print k
 
