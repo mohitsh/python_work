0c6a8e8aafdccd6809c44012de15bf4c1feb4e80
mohitsh <mohitsh114@gmail.com>
2015-09-21 15:47:16 +0530

    dfs4.py

--- graph/dfs4.py
+++ graph/dfs4.py
@@ -1,24 +1,24 @@
 from graph10 import Graph
 
-g = Graph(directed = True)
+g = Graph()
 
 v1 = g.insert_vertex("v1")
 v2 = g.insert_vertex("v2")
 v3 = g.insert_vertex("v3")
 v4 = g.insert_vertex("v4")
-#v5 = g.insert_vertex("e")
-#v6 = g.insert_vertex("f")
-#v7 = g.insert_vertex("g")
+v5 = g.insert_vertex("v5")
+v6 = g.insert_vertex("v6")
+v7 = g.insert_vertex("v7")
 #v8 = g.insert_vertex("h")
 
 e1 = g.insert_edge(v1,v2,10)
-e2 = g.insert_edge(v3,v1,20)
-e3 = g.insert_edge(v1,v4,30)
-e4 = g.insert_edge(v3,v2,40)
+e2 = g.insert_edge(v1,v3,20)
+e3 = g.insert_edge(v3,v4,30)
+e4 = g.insert_edge(v1,v4,40)
 e5 = g.insert_edge(v2,v3,50)
-#e6 = g.insert_edge(v1,v4,60)
-e7 = g.insert_edge(v4,v3,70)
-e8 = g.insert_edge(v3,v4,80)
+e6 = g.insert_edge(v5,v7,60)
+e7 = g.insert_edge(v6,v7,70)
+e8 = g.insert_edge(v5,v6,80)
 #e9 = g.insert_edge(v4,v7,90)
 #e10 = g.insert_edge(v4,v8,100)
 
@@ -45,7 +45,7 @@ def construct_path(u,v,dic):
 
 
 def dfs1(g,v,dic):
-	for e in g.incident_edge(v,outgoing=False):
+	for e in g.incident_edge(v):
 		u = e.opposite(v)
 		if u not in dic:
 			dic[u] = e
@@ -67,37 +67,27 @@ def construct_path1(u,v,dic):
 	return path
 
 dic = {}
-dude = dfs1(g,v4,dic)
+dude = dfs1(g,v5,dic)
 print "dictionary ->"
 for key in dic:
 	print key,dic[key]
 
-#print "path b/w v1 and v7 ->"
-#path = construct_path1(v1,v4,dude)
-#for v in path:
-#	print v
+print "path b/w v1 and v7 ->"
+path = construct_path1(v5,v7,dude)
+for v in path:
+	print v
 
+# connected components
 
+print "no of connected components are equal to vertices having None as edge"
+def dfs_complete(g):
+	forest = {}
+	for u in g.vertices():
+		if u not in forest:
+			forest[u] = None
+			dfs(g,u,forest)
+	return forest
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+forest = dfs_complete(g)
+for k in forest:
+	print k,forest[k]
