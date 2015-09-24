237f036f2e4efe3421c1c769525b97e7126121d8
mohitsh <mohitsh114@gmail.com>
2015-09-24 20:26:11 +0530

    mergesor1.py

---
+++ graph/mergesor1.py
@@ -0,0 +1,41 @@
+
+def merge(alist):
+	if len(alist) > 1:
+		mid = len(alist)//2
+		left = alist[:mid]
+		right = alist[mid:]
+		
+		merge(left)
+		merge(right)
+		
+		i = 0
+		j = 0
+		k = 0
+		
+		while i < len(left) and j < len(right):
+			if left[i] < right[j]:
+				alist[k] = left[i]
+				i = i + 1
+			else:
+				alist[k] = right[j]
+				j = j + 1
+			k = k + 1
+		while i < len(left):
+			alist[k] = left[i]
+			i = i + 1 	
+			k = k + 1
+		while j < len(right):
+			alist[k] = right[j]
+			j = j + 1
+			k = k + 1
+		
+alist = [5,4,3,2,1]
+print alist
+merge(alist)
+print alist
+
+alist = [54,26,93,17,77,31,44,55,20]
+print alist
+merge(alist)
+print alist
+		
