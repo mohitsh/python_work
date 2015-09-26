cbecb475bc99fdce2b2f394ad14e84955aaae44e
mohitsh <mohitsh114@gmail.com>
2015-09-26 23:55:59 +0530

    largRec.py

--- graph/largRec.py
+++ graph/largRec.py
@@ -22,23 +22,6 @@ class Stack:
 n = int(raw_input())
 h1 = raw_input().split()
 h = [int(x) for x in h1]
+print h
 
-#print h
 
-ans = [-1]
-for i in range(len(h)):
-	s = Stack()
-	for k in range(i,-1,-1):
-		if h[k] >= h[i]:
-			s.push(h[k]) 
-		else:
-			break
-	for j in range(i+1,len(h)):
-		if h[j] >= h[i]:
-			s.push(h[j])
-		else:
-			break
-	if h[i]*len(s) > ans[-1]:
-		ans.append(h[i]*(len(s)))
-	
-print ans[-1]
