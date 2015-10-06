bf756f792ed49947b3363030fec802d81abf7688
mohitsh <mohitsh114@gmail.com>
2015-10-06 19:13:21 +0530

    choco.py

--- leet/choco.py
+++ leet/choco.py
@@ -1,11 +1,22 @@
+ 
+# this tiny took me 3 hours
+# so I am going to explain everything here
+
 
 def choco(n,c,m):
+	# chocolate you bought with all your money
 	bought = n/c
+	# keep the count of chocolates bought
 	count = bought
+	# as long you have chocolates to exchange
 	while bought >= m:
+		# this is the number you can exchange
 		exchange = (bought/m)*m 
+		# chocolates you get in exchange
 		choco = (bought/m)
+		# increase your chocolate count
 		count = count + choco
+		# this is the amount of chocolates left after exchanging 
 		bought = bought-exchange+choco
 	return count
 
