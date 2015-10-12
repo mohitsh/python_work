4586c843006985b6d8972a4450139883d2990e15
mohitsh <mohitsh114@gmail.com>
2015-10-13 01:48:07 +0530

    cluster.py

---
+++ thesis/cluster.py
@@ -0,0 +1,11 @@
+import csv
+import numpy as np
+import scipy
+import scipy.cluster
+
+reader = csv.reader(open("red_data2.csv","rb"),delimiter=",")
+x = list(reader)
+result = np.array(x).astype('float')
+np.shape(result)
+dude = scipy.cluster.vq.kmeans2(result,k=80)
+print dude
